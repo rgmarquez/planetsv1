352623cde9c823a44a653f5e85a0718eee835798
Greg Marquez <gmarquezjm@yahoo.com>
2024-02-15 10:45:22 -0800

    Cleaned up script.js and planet.js

--- js/modules/planet.js
+++ js/modules/planet.js
@@ -9,6 +9,10 @@ class ImpulseAggregator {
     this.impulses.push({'dx' : dx, 'dy' : dy});
   }
 
+  clearAcceleration() {
+    this.impulses.length = 0;
+  }
+
   resolveAcceleration() {
     let ax = 0;
     let ay = 0;
@@ -16,7 +20,7 @@ class ImpulseAggregator {
         ax += impulse.dx;
         ay += impulse.dy;
     });
-    this.impulses.length = 0;
+    this.clearAcceleration();
     return ({'ax' : ax, 'ay' : ay});
   }
 }
@@ -95,27 +99,17 @@ class Planet {
 
   // class (static) methods
   static computePhysics(obj1, obj2) {
-    var d = distance(obj1, obj2);
-    if (Math.abs(d) > 2.0) {
+    const d = distance(obj1, obj2);
+    if (d > 0) {
       const dx = obj1.x - obj2.x;
       const dy = obj1.y - obj2.y;
-  
       const force = (obj1.mass + obj2.mass) / (d * d)
-  
       const ax = (dx/d) * force;
       const ay = (dy/d) * force;
-  
       const obj1MassRatio = obj1.mass / (obj1.mass + obj2.mass);
       const obj2MassRatio = obj2.mass / (obj1.mass + obj2.mass);
-  
       obj1.aggregateAcceleration.addAcceleration(-ax * obj2MassRatio, -ay * obj2MassRatio);
       obj2.aggregateAcceleration.addAcceleration(ax * obj1MassRatio, ay * obj1MassRatio);
-    }
-    else {
-      obj1.setVx(0);
-      obj1.setVy(0);
-      obj2.setVx(0);
-      obj2.setVy(0);
     }
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
 import { Planet } from "./modules/planet.js";
 import { clearScreen, resizeCanvas } from "./modules/utils.js";
 
-var jupiterToSunMassRatio = 0.00095479194;
-var earthToSunMassRatio = 0.00000300348959632;
-var marsToSunMassRatio = 0.107 * earthToSunMassRatio;
-var nemesisToSunMassRatio = 0.05;
-var initialSunMass = 150;
-
-var sunInit = {'name' : 'sun', 'fill' : 'yellow', 'outline' : 'orange', 'radius' : 18.0, 'mass' : initialSunMass,'dx' : 0.0, 'dy' : -0.00, 'vx' : 0.0, 'vy' : 0.0};
-var earthInit = {'name' : 'earth', 'fill' : 'white', 'outline' : 'blue', 'radius' : 2.0, 'mass' : earthToSunMassRatio * initialSunMass, 'dx' : 0.0, 'dy' : 60.0, 'vx' : 1.55, 'vy' : 0.0};
-var marsInit = {'name' : 'mars', 'fill' : 'red', 'outline' : 'darkred', 'radius' : 2.0, 'mass' : marsToSunMassRatio * initialSunMass, 'dx' : 0.0, 'dy' : 100.0, 'vx' : 1.20, 'vy' : 0.0};
-var planetXInit = {'name' : 'planet-x', 'fill' : 'yellow', 'outline' : 'orange', 'radius' : 4.0, 'mass' : jupiterToSunMassRatio * initialSunMass,'dx' : 0.0, 'dy' : -250.0, 'vx' : -0.75, 'vy' : 0.0};
-var nemesisInit = {'name' : 'nemesis', 'fill' : 'black', 'outline' : 'red', 'radius' : 7.0, 'mass' : nemesisToSunMassRatio * initialSunMass, 'dx' : -725.0, 'dy' : 0.0, 'vx' : 0.10, 'vy' : 0.27};
-
+window.requestAnimFrame = function( callback ){
+  window.setTimeout(callback, 1000 / 100);
+};
 window.requestAnimFrame = window.requestAnimationFrame
 
 var winHeight = $(window).height();
@@ -23,33 +14,50 @@ const context = canvas.getContext('2d');
 clearScreen(canvas, context);
 let centerOfScreen = resizeCanvas(canvas, context);
 
-const sun = new Planet(sunInit);
-const earth = new Planet(earthInit);
-const mars = new Planet(marsInit);
-const planetX = new Planet(planetXInit);
-const nemesis = new Planet(nemesisInit);
-const solarSystem = [sun, earth, mars, planetX, nemesis];
-solarSystem.centerReferenceObject = sun;
+var jupiterToSunMassRatio = 0.00095479194;
+var saturnToSunMassRatio = jupiterToSunMassRatio * 0.3;
+var earthToSunMassRatio = 0.00000300348959632;
+var marsToSunMassRatio = 0.107 * earthToSunMassRatio;
+var nemesisToSunMassRatio = 0.04;
+var initialSunMass = 150;
 
-function tick(starSystem) {
+const starSystemInit = [
+  {name: 'sun', fill: 'yellow', outline: 'orange', radius: 18.0, mass: initialSunMass, dx: 0.0, dy: -0.00, vx: 0.0, vy: 0.0},
+  {name: 'venus', fill: 'white', outline: 'green', radius: 2.0, mass : earthToSunMassRatio * initialSunMass, dx: 0.0, dy: 30.0, vx: 2.1, vy: 0.0},
+  {name: 'earth', fill: 'white', outline: 'blue', radius: 2.0, mass: earthToSunMassRatio * initialSunMass, dx: 0.0, dy: 60.0, vx: 1.55, vy: 0.0},
+  {name: 'mars', fill: 'white', outline: 'darkred', radius: 2.0, mass: marsToSunMassRatio * initialSunMass, dx: 0.0, dy: 100.0, vx: 1.20, vy: 0.0},
+  {name: 'jupiter', fill: 'yellow', outline: 'orange', radius: 4.0, mass: jupiterToSunMassRatio * initialSunMass,dx: 0.0, dy: -250.0, vx: -0.75, vy: 0.0},
+  {name: 'saturn', fill: 'orange', outline: 'yellow', radius: 3.0, mass: saturnToSunMassRatio * initialSunMass,dx: 0.0, dy: -350.0, vx: -0.65, vy: 0.0},
+  {name: 'nemesis', fill: 'black', outline: 'red', radius: 7.0, mass: nemesisToSunMassRatio * initialSunMass, dx: -725.0, dy: 0.0, vx: 0.0, vy: 0.40}
+]
+
+const starSystem = starSystemInit.map((x) => new Planet(x))
+starSystem.centerReferenceObject = starSystem.find((x) => x.name === 'sun')
+
+function tick() {
   requestAnimFrame(tick);
 
-  solarSystem.forEach( function(starSystemBody) {
-    starSystemBody.tick();
-    starSystemBody.draw(context, solarSystem.centerReferenceObject, centerOfScreen);
-  });
-
-  var numberOfBodies = solarSystem.length;
-  for (var i = 0; i < (numberOfBodies - 2); ++i) {
-    for (var j = i + 1; j < numberOfBodies; ++j) {
-      Planet.computePhysics(solarSystem[i], solarSystem[j]);
+  for (let times = 0; times < 4; ++times) {
+    starSystem.forEach( function(starSystemObject) {
+      starSystemObject.tick();
+      starSystemObject.draw(context, starSystem.centerReferenceObject, centerOfScreen);
+    });
+  
+    const numberOfBodies = starSystem.length;
+    if (numberOfBodies) {
+      for (var i = 0; i < numberOfBodies - 1; ++i) {
+        for (var j = i + 1; j < numberOfBodies; ++j) {
+          Planet.computePhysics(starSystem[i], starSystem[j]);
+        }
+      }
     }
+  
+    starSystem.forEach( function(starSystemObject) {
+      starSystemObject.resolveAcceleration();
+    });
   }
-  solarSystem.forEach( function(starSystemBody) {
-    starSystemBody.resolveAcceleration();
-  });
 }
 
 $( document ).ready(function() {
-  tick(solarSystem);
+  tick();
 });
